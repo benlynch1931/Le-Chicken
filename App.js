f3018a4dd404f42b915fa24a4c537dd87c7f1aaf
Emily Sesto <emily@imac.home>
2020-12-09 11:44:53 +0000

    Add text input

--- App.js
+++ App.js
@@ -1,17 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 import Chicken from './src/Chicken.js'
 import Coop from './src/Coop.js'
 
 export default function App() {
   const [chickenTop, setChickenTop] = useState(100)
+
+  const checkInput = (text) => {
+    if (text.toLowerCase() === "marche") {
+      alert("Correct!")
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text>Le Chicken!</Text>
       <StatusBar style="auto" />
       <Coop></Coop>
       <Chicken>chickenTop={chickenTop}</Chicken>
+      <TextInput style = {styles.input}
+               placeholder = "Type Marche"
+               placeholderTextColor = "black"
+               autoCapitalize = "none"
+               name = "marche"
+               onChangeText = {checkInput}
+               />
     </View>
   );
 }
