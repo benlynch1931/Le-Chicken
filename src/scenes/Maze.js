73fa2370f801ccbd782c552d1a4be7925dc501db
Ben <ben.lynch1931@gmail.com>
2020-12-11 15:36:14 +0000

    convert maze inner dimensions to percentages

--- src/scenes/Maze.js
+++ src/scenes/Maze.js
@@ -18,24 +18,24 @@ const Maze = () => {
     {/* The walls for the maze */}
     <Svg height={hp("49.26%")} width={wp('100%')} style={{ position: "absolute", top: hp("1.85%")}}>
       {/* borders */}
-        <Line x1="212.5"   x2="375"   y1="392.5"  y2="392.5"  stroke="#38761D" strokeWidth="15" />
-        <Line x1="0"       x2="162.5" y1="392.5"  y2="392.5"  stroke="#38761D" strokeWidth="15" />
-        <Line x1="7.5"     x2="7.5"   y1="0"      y2="400"    stroke="#38761D" strokeWidth="15" />
-        <Line x1="367.5"   x2="367.5" y1="0"      y2="400"    stroke="#38761D" strokeWidth="15" />
-        <Line x1="0"       x2="65"    y1="7.5"    y2="7.5"    stroke="#38761D" strokeWidth="15" />
-        <Line x1="115"     x2="375"   y1="7.5"    y2="7.5"    stroke="#38761D" strokeWidth="15" />
-      {/* inside right */}
-        <Line x1="155"     x2="155"   y1="335"    y2="400"    stroke="#38761D" strokeWidth="15" />
-        <Line x1="147.5"   x2="310"   y1="327.5"  y2="327.5"  stroke="#38761D" strokeWidth="15" />
-        <Line x1="302.5"   x2="302.5" y1="65"     y2="320"    stroke="#38761D" strokeWidth="15" />
-        <Line x1="230"     x2="310"   y1="72.5"   y2="72.5"   stroke="#38761D" strokeWidth="15" />
-        <Line x1="237.5"   x2="237.5" y1="65"     y2="190"    stroke="#38761D" strokeWidth="15" />
-      {/* inside left */}
-        <Line x1="172.5"   x2="172.5" y1="0"      y2="253"    stroke="#38761D" strokeWidth="15" />
-        <Line x1="65"      x2="245"   y1="260"    y2="260"    stroke="#38761D" strokeWidth="15" />
-        <Line x1="81.25"   x2="81.25" y1="252.5"  y2="335"    stroke="#38761D" strokeWidth="32.5" />
-        <Line x1="0"       x2="65"    y1="195"    y2="195"    stroke="#38761D" strokeWidth="15" />
-        <Line x1="115"     x2="180"   y1="227.5"  y2="227.5"  stroke="#38761D" strokeWidth="80" />
+      <Line x1={wp("56.67%")} x2={wp("100.00%")} y1={hp("48.34%")} y2={hp("48.34%")} stroke="#38761D" strokeWidth={hp("1.85%")} />
+<Line x1={wp("0.00%")} x2={wp("43.33%")} y1={hp("48.34%")} y2={hp("48.34%")} stroke="#38761D" strokeWidth={hp("1.85%")} />
+<Line x1={wp("2.00%")} x2={wp("2.00%")} y1={hp("0.00%")} y2={hp("49.26%")} stroke="#38761D" strokeWidth={wp("4.00%")} />
+<Line x1={wp("98.00%")} x2={wp("98.00%")} y1={hp("0.00%")} y2={hp("49.26%")} stroke="#38761D" strokeWidth={wp("4.00%")} />
+<Line x1={wp("0.00%")} x2={wp("17.33%")} y1={hp("0.92%")} y2={hp("0.92%")} stroke="#38761D" strokeWidth={hp("1.85%")} />
+<Line x1={wp("30.67%")} x2={wp("100.00%")} y1={hp("0.92%")} y2={hp("0.92%")} stroke="#38761D" strokeWidth={hp("1.85%")} />
+{/* inside right */}
+<Line x1={wp("41.33%")} x2={wp("41.33%")} y1={hp("41.26%")} y2={hp("49.26%")} stroke="#38761D" strokeWidth={wp("4.00%")} />
+<Line x1={wp("39.33%")} x2={wp("82.67%")} y1={hp("40.33%")} y2={hp("40.33%")} stroke="#38761D" strokeWidth={hp("1.85%")} />
+<Line x1={wp("80.67%")} x2={wp("80.67%")} y1={hp("8.00%")} y2={hp("39.41%")} stroke="#38761D" strokeWidth={wp("4.00%")} />
+<Line x1={wp("61.33%")} x2={wp("82.67%")} y1={hp("8.93%")} y2={hp("8.93%")} stroke="#38761D" strokeWidth={hp("1.85%")} />
+<Line x1={wp("63.33%")} x2={wp("63.33%")} y1={hp("8.00%")} y2={hp("23.40%")} stroke="#38761D" strokeWidth={wp("4.00%")} />
+{/* inside left */}
+<Line x1={wp("46.00%")} x2={wp("46.00%")} y1={hp("0.00%")} y2={hp("31.16%")} stroke="#38761D" strokeWidth={wp("4.00%")} />
+<Line x1={wp("17.33%")} x2={wp("65.33%")} y1={hp("32.02%")} y2={hp("32.02%")} stroke="#38761D" strokeWidth={hp("1.85%")} />
+<Line x1={wp("21.67%")} x2={wp("21.67%")} y1={hp("31.10%")} y2={hp("41.26%")} stroke="#38761D" strokeWidth={wp("8.67%")} />
+<Line x1={wp("0.00%")} x2={wp("17.33%")} y1={hp("24.01%")} y2={hp("24.01%")} stroke="#38761D" strokeWidth={hp("1.85%")} />
+<Line x1={wp("30.67%")} x2={wp("48.00%")} y1={hp("28.02%")} y2={hp("28.02%")} stroke="#38761D" strokeWidth={hp("9.85%")} />
     </Svg>
 
     </View>
