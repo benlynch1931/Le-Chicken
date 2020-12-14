59a2cbaf8979f9a304beae3e94ae2cd53e1269aa
Emily Sesto <emily@imac.home>
2020-12-14 22:25:41 +0000

    Align text in AttackCommands

--- src/AttackCommands.js
+++ src/AttackCommands.js
@@ -4,24 +4,59 @@ import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-nat
 
 const AttackCommands = (props) => {
   const [inputText, setInputText] = useState("")
+  const [battleReport, setBattleReport] = useState("")
+  const [chickenTurn, setChickenTurn] = useState(true)
+  const [result, setResult] = useState("")
 
   const checkInput = (text) => {
     setInputText(text)
-    if (text.toLowerCase() == "frapper") {
-        setInputText("")
-        props.setOpponentHealth(props.opponentHealth - (Math.floor(Math.random() * 6) + 5))
-    }
+    if(chickenTurn == true) {
+        console.log(props.chickenHealth, props.opponentHealth)
+        if (text.toLowerCase() == "frapper") {
+            setInputText("")
+            chickenAttack()
+        }
+    }  
   }
 
+  const chickenAttack = () => {
+    props.setOpponentHealth(props.opponentHealth - (Math.floor(Math.random() * 6) + 20))
+    setBattleReport("Le chicken a frappé l’adversaire")
+    checkHealth()
+    setChickenTurn(false)
+    opponentTurn()
+  }
+
+  const opponentTurn = () => {
+      setTimeout(() => {
+        props.setChickenHealth(props.chickenHealth - (Math.floor(Math.random() * 6) + 20))
+        setBattleReport("L’adversaire a frappé le chicken")
+        checkHealth()
+        setChickenTurn(true)
+      }, 2000)
+  }
+
+  const checkHealth = () => {
+      if(chickenTurn) {
+        if(props.opponentHealth <= 20) {
+            setResult("You won")
+        }
+      }else {
+          if(props.chickenHealth <= 20) {
+            setResult("You lost")
+          }
+      }
+  }
   return (
-    <View>
-      <Text>Frapper</Text>
-      <TextInput
+    <View style={styles.container}>
+      <Text style={styles.hintText}>Hint: Frapper</Text>
+      <Text style={styles.hintText}>{`${battleReport}`}</Text>
+      <Text style={styles.hintText}>{`${result}`}</Text>
+      <TextInput style={styles.input}
       placeholderTextColor="black"
       testID="textInput"
       onChangeText={checkInput}
       value={inputText}
-      style={styles.input}
       ></TextInput>
     </View>
   )
@@ -32,6 +67,10 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
     width: wp('100%'),
+  },
+  hintText: {
+    alignSelf: "center",
+    padding: hp("2%")
   },
   input: {
     width: wp("40%"),
