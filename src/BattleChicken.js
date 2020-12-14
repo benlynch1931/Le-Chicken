3efa6c325865259e2153b88138d5863e37a5610c
Emily Sesto <emily@imac.home>
2020-12-14 22:25:19 +0000

    Add battleChicken entrance animation

--- src/BattleChicken.js
+++ src/BattleChicken.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Image } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 
 const BattleChicken = () => {
+  const [chickenPosition, setChickenPosition] = useState(0)
   const chickenWidth = wp("13.33%")
   const chickenHeight = hp("6.16%")
+
+  useEffect(() => {
+    if(chickenPosition < 80) {
+        setChickenPosition(chickenPosition => chickenPosition + 0.25)
+    }
+  }, [chickenPosition])
 
   return (
     < Image
       style={{
-        position: 'absolute',
-        top: 300,
-        left: 100,
+        position: `wp('${chickenPosition}%')`,
+        top: hp("60%"),
+        left: chickenPosition,
         width: chickenWidth,
         height: chickenHeight,
         zIndex: 4
@@ -24,3 +31,4 @@ const BattleChicken = () => {
 }
 
 export default BattleChicken
+
