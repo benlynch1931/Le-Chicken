300452d4c3a9740ee43c09a9cf287fd5d60dedfb
charlie-galb <galbraith.charlie@gmail.com>
2020-12-14 15:14:45 +0000

    Add styling

--- src/AttackCommands.js
+++ src/AttackCommands.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { View, TextInput, Text } from 'react-native';
+import { View, TextInput, Text, StyleSheet } from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 const AttackCommands = (props) => {
   const [inputText, setInputText] = useState("")
@@ -20,9 +21,28 @@ const AttackCommands = (props) => {
       testID="textInput"
       onChangeText={checkInput}
       value={inputText}
+      style={styles.input}
       ></TextInput>
     </View>
   )
 
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    width: wp('100%'),
+  },
+  input: {
+    width: wp("40%"),
+    fontSize: 20,
+    borderWidth: 2,
+    borderColor: 'grey',
+    padding: 10,
+    borderRadius: 5,
+    textAlign: "center",
+    alignSelf: "center"
+  }
+});
+
 export default AttackCommands;
