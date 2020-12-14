0f51bda34f274d73d1efeaff6dbadec85018069f
Emily Sesto <emily@imac.home>
2020-12-14 22:48:40 +0000

    Add battle opponent entrance animation

--- src/BattleOpponent.js
+++ src/BattleOpponent.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Image } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
 
 const BattleOpponent = () => {
+  const [opponentPosition, setOpponentPosition] = useState(90)
   const chickenWidth = wp("13.33%")
   const chickenHeight = hp("6.16%")
+  let entrance;
+
+  useEffect(() => {
+    if(opponentPosition > 60) {
+        entrance = setInterval(() => {
+            setOpponentPosition(opponentPosition => opponentPosition - 2)
+        }, 80)
+        return () => {
+            clearInterval(entrance)
+        }
+    }
+  }, [opponentPosition])
 
   return (
     < Image
       style={{
         position: 'absolute',
-        top: 100,
-        left: 300,
+        top: hp("20%"),
+        left: wp(opponentPosition),
         width: chickenWidth,
         height: chickenHeight,
         zIndex: 4
