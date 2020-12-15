f45581ecf6681bf66d5bcaa48644e04e3301be91
emilyalice2708 <emilyalicesesto@gmail.com>
2020-12-15 13:32:04 +0000

    Add battleChicken test to check end position

--- src/scenes/battle/BattleChicken.test.js
+++ src/scenes/battle/BattleChicken.test.js
@@ -4,11 +4,21 @@ import BattleChicken from './BattleChicken.js';
 import { BattleContext } from '../../contexts/BattleContext.js';
 
 const mockContext = {
-    battleChickenPosition: [0, 0],
+    battleChickenPosition: [20, 0],
     changeBattleChickenPosition: jest.fn() 
 }
 
+test('chicken is not moved past wp("20%")', async () => {
+    jest.useFakeTimers();
+    await act(async () => {
+      newBattleChicken = create(<BattleContext.Provider value={mockContext}><BattleChicken /></BattleContext.Provider>)
+    });
+    jest.advanceTimersByTime(3000);
+    expect(mockContext.changeBattleChickenPosition).not.toHaveBeenCalled();
+});
+
 test('chicken position is changed on render', async () => {
+    mockContext.battleChickenPosition = [0, 0]
     jest.useFakeTimers();
     await act(async () => {
       newBattleChicken = create(<BattleContext.Provider value={mockContext}><BattleChicken /></BattleContext.Provider>)
