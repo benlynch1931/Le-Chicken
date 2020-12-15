bd3ad60eade1065de4810dbbd841e279456313d9
emilyalice2708 <emilyalicesesto@gmail.com>
2020-12-15 12:52:46 +0000

    Update attackCommands test to use mockContext

--- src/scenes/battle/AttackCommands.test.js
+++ src/scenes/battle/AttackCommands.test.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import AttackCommands from './AttackCommands.js';
 import { render, fireEvent } from '@testing-library/react-native'
+import Battle from './Battle.js';
+import { BattleContext } from '../../contexts/BattleContext.js';
 
 const mockContext = {
-    changeChickenDirection: jest.fn()
+    chickenHealth: 100, 
+    changeResult: jest.fn(),
+    opponentHealth: 50,
+    inputText: "",
+    battleReport: "",
+    changeBattleReport: jest.fn(),
+    chickenTurn: true,
+    result: "",
+    displayResult: jest.fn(),
+    changeChickenTurn: jest.fn(),
+    changeOpponentHealth: jest.fn(),
+    changeChickenHealth: jest.fn(),
+    changeInputText: jest.fn()
 }
-
-mockSetChickenHealth = jest.fn()
-mockSetOpponentHealth = jest.fn()
 
 beforeEach(() => {
     jest.spyOn(global.Math, 'random').mockReturnValue(1);
@@ -18,23 +29,17 @@ afterEach(() => {
 })
 
 test("Changes input but not level when incorrect input is given", () => {
-    const { getByTestId } = render(<AttackCommands
-        chickenHealth={100} 
-        setChickenHealth={mockSetChickenHealth}
-        opponentHealth={100}
-        setOpponentHealth={mockSetOpponentHealth}/>)
+    const { getByTestId } = render(<BattleContext.Provider value={mockContext}><AttackCommands/></BattleContext.Provider>)
     const input = getByTestId('textInput')
     fireEvent.changeText(input, "incorrect string")
-    expect(mockSetOpponentHealth).not.toHaveBeenCalled();
+    expect(mockContext.changeInputText).toHaveBeenCalled();
 })
 
 test("Changes input but not level when incorrect input is given", () => {
-    const { getByTestId } = render(<AttackCommands
-        chickenHealth={100} 
-        setChickenHealth={mockSetChickenHealth}
-        opponentHealth={100}
-        setOpponentHealth={mockSetOpponentHealth}/>)
+    const { getByTestId } = render(<BattleContext.Provider value={mockContext}><AttackCommands/></BattleContext.Provider>)
     const input = getByTestId('textInput')
     fireEvent.changeText(input, "frapper")
-    expect(mockSetOpponentHealth).toHaveBeenCalledWith(100 - 26);
+    console.log(input)
+    expect(mockContext.changeInputText).toHaveBeenCalled();
+
 })
