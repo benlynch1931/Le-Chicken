9b80ae1e47fe43d93ef7a29146b7231c73d04ef6
KC Thomas <thisiskcthomas@gmail.com>
2020-12-15 14:11:53 +0000

    Change DPad to class component. Not Functional/Working on it

--- src/DPad.js
+++ src/DPad.js
@@ -1,80 +1,66 @@
-import React, { useContext } from 'react';
+import React, { useContext, Component } from 'react';
 import { TextInput, Text, View, TouchableOpacity, Button, StyleSheet, Keyboard } from 'react-native';
 import { GameContext } from './contexts/GameContext.js';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
-
-const DPad = () => {
-  const { addToDictionary, changeInputText, changeCoopGraphic, inputText, changeLevel, level, changeChickenToMove, changeChickenDirection, chickenDirection } = useContext(GameContext)
-
-  const directions = new Map()
-  directions.set('haut', 'up')
-  directions.set('bas', 'down')
-  directions.set('droite', 'right')
-  directions.set('gauche', 'left')
-
-
-  // const checkInput = (text) => {
-  //   changeInputText(text)
-  //   if (level === 0 && text.toLowerCase() === "marcher") {
-  //     Keyboard.dismiss();
-  //     changeLevel(1)
-  //     sendToDictionary('Marcher', 'To walk');
-  //     changeChickenToMove(90);
-  //     changeInputText("")
-  //   }
-
-  //   if (level === 1 && text.toLowerCase() === "ouvrir") {
-  //     Keyboard.dismiss();
-  //     changeLevel(2);
-  //     sendToDictionary('Ouvrir', 'To open');
-  //     sendToDictionary('Haut', 'Up');
-  //     sendToDictionary('Bas', 'Down');
-  //     sendToDictionary('Gauche', 'Left');
-  //     sendToDictionary('Droite', 'Right');
-  //     changeChickenToMove(27);
-  //     changeInputText("");
-  //   }
-
-  //   if (level === 2) {
-  //     for (const [french, english] of directions.entries()) {
-  //       if (text.toLowerCase() == french) {
-  //         changeChickenDirection(english);
-  //         changeChickenToMove(100);
-  //         changeInputText("");
-  //       }
-  //     }
-  //   }
-  // }
-  let chickenMoving = false
-
-  const moveChicken = (direction, num) => {
-    chickenMoving = true
-    if(chickenMoving == true){
-      changeChickenDirection(direction)
-      changeChickenToMove(num)
-    }
+export class DPad extends Component {
+  constructor() {
+    super();
+    this.state = {
+      number: 0,
+    };
+    this.direction = null;
+    this.timer = null;
+    this.addOne = this.addOne.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);  
   }
 
-  return (
-    < View style={styles.container} >
-      {/* <TextInput style={styles.input}
-        placeholderTextColor="black"
-        testID="textInput"
-        onChangeText={checkInput}
-        value={inputText} */}
-      {/* /> */}
-      {/* <Button
-          onPressIn={debugMe()}
-          onPressOut={debugMe()}
-          title="HAUT"
-          color="#841584"
-        /> */}
-      <TouchableOpacity onPressIn={ () => moveChicken('up', 1)} onPressOut={ () => {chickenMoving = false}}>
-        <Text>HAUT</Text>
-      </TouchableOpacity>
-    </View >
-  )
+  addOne() {
+    const { changeChickenToMove, changeChickenDirection } = useContext(GameContext)
+
+    this.setState({number: this.state.number+1});
+    changeChickenDirection(this.direction);
+    changeChickenToMove(this.state.number);
+    this.timer = setTimeout(this.addOne, 200);
+  }
+
+  stopTimer() {
+    clearTimeout(this.timer);
+  }
+
+  // var myVar = setInterval(myTimer, 1000);
+  // let direction = null
+
+  // const myTimer = () => {
+  //   changeChickenDirection(direction)
+  //   changeChickenToMove(1)
+  // }
+
+  // const myStopFunction= () => {
+  //   clearInterval(myVar);
+  // }
+
+  render() {
+    return (
+      < View style={styles.container} >
+        {/* <TextInput style={styles.input}
+          placeholderTextColor="black"
+          testID="textInput"
+          onChangeText={checkInput}
+          value={inputText} */}
+        {/* /> */}
+        {/* <Button
+            onPressIn={debugMe()}
+            onPressOut={debugMe()}
+            title="HAUT"
+            color="#841584"
+          /> */}
+        <TouchableOpacity onPressIn={ this.direction = 'up', this.addOne  } onPressOut={ this.stopTimer }>
+          <Text>HAUT</Text>
+        </TouchableOpacity>
+      </View >
+    )
+  }
 }
 
 const styles = StyleSheet.create({
