055ae0a8a11d2267c4d694d6c4a1e878d5b940b9
emilyalice2708 <emilyalicesesto@gmail.com>
2020-12-15 18:16:23 +0000

    Remove comments from GameContext

--- src/contexts/GameContext.js
+++ src/contexts/GameContext.js
@@ -5,11 +5,10 @@ import { walls } from '../Walls.js';
 export const GameContext = createContext();
 
 const chickenPositionDefault = [wp("43.34%"), hp("60%")];
-const initialScene = 'battle';
+const initialScene = 'confrontation';
 const initialChickenGraphic = 'idleright';
 const initialHint = "Pour marcher: Type ‘marcher’";
 const initialLevel = 0;
-// const initialGameMode = 'game';
 
 class GameContextProvider extends Component {
   state = {
@@ -20,7 +19,6 @@ class GameContextProvider extends Component {
     hint: initialHint,
     chickenToMove: 0,
     level: initialLevel,
-    // gameMode: initialGameMode,
     chickenDirection: "up",
     translations: [],
     walls: walls
@@ -34,9 +32,7 @@ class GameContextProvider extends Component {
   changeLevel = (level) => {
     this.setState({ level: level })
   }
-  // changeGameMode = (mode) => {
-  //   this.setState({ gameMode: mode})
-  // }
+
   changeChickenDirection = (direction) => {
     this.setState({ chickenDirection: direction })
   }
@@ -89,7 +85,6 @@ class GameContextProvider extends Component {
         changeChickenDirection: this.changeChickenDirection,
         changeLevel: this.changeLevel,
         restartGame: this.restartGame,
-        // changeGameMode: this.changeGameMode,
         addToDictionary: this.addToDictionary
       }}>
         {this.props.children}
