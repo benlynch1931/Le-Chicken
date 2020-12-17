236cddc87293af818ba613e7f00e0275e8f96349
Dan Holmes <dh@danielholmes.com>
2020-12-17 15:22:15 +0000

    Reset battle context on new game

--- src/Menu.js
+++ src/Menu.js
@@ -2,14 +2,16 @@ import React, { useContext, useEffect, useState } from 'react';
 import { StyleSheet, View, Text, Button, TouchableOpacity, Image } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import { GameContext } from './contexts/GameContext.js';
+import { BattleContext } from './contexts/BattleContext.js';
 import { useFonts } from 'expo-font';
 
 const Menu = (props) => {
 
   const { restartGame } = useContext(GameContext)
+  const { resetBattleContext } = useContext(BattleContext)
 
   const [gameStarted, setGameStarted] = useState(false)
-  if(props.view != 'menu'){ return null}
+  if (props.view != 'menu') { return null }
   const startNewGame = async () => {
     restartGame();
     await setGameStarted(true);
@@ -23,13 +25,13 @@ const Menu = (props) => {
 
   const continueGameButton = () => {
     if (gameStarted) {
-      return(
-      <TouchableOpacity
+      return (
+        <TouchableOpacity
           // style={styles.menuButton}
           onPress={() => continueGame()}
-      >
-        <Text style={{fontFamily: 'Pixel', alignSelf: 'center', marginTop: 30}}>Continue Game</Text>
-      </TouchableOpacity>
+        >
+          <Text style={{ fontFamily: 'Pixel', alignSelf: 'center', marginTop: 30 }}>Continue Game</Text>
+        </TouchableOpacity>
       )
     }
   }
@@ -44,12 +46,12 @@ const Menu = (props) => {
         width: wp("100%")
       }} />
       <TouchableOpacity
-          onPress={() => startNewGame()}
+        onPress={() => startNewGame()}
       >
-        <Text style={{fontFamily: 'Pixel', fontSize: 30, alignSelf: 'center'}}>Le Chicken:</Text>
-        <Text style={{fontFamily: 'Pixel', fontSize: 30, alignSelf: 'center'}}>The Poulet</Text>
+        <Text style={{ fontFamily: 'Pixel', fontSize: 30, alignSelf: 'center' }}>Le Chicken:</Text>
+        <Text style={{ fontFamily: 'Pixel', fontSize: 30, alignSelf: 'center' }}>The Poulet</Text>
 
-        <Text style={{fontFamily: 'Pixel', alignSelf: 'center', marginTop: 30}}>New Game</Text>
+        <Text style={{ fontFamily: 'Pixel', alignSelf: 'center', marginTop: 30 }}>New Game</Text>
       </TouchableOpacity>
       { continueGameButton()}
     </View>
