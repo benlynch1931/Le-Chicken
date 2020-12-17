f43e934306e1c667516c4a9ad7e65b0838a0f8af
Dan Holmes <dh@danielholmes.com>
2020-12-17 09:58:05 +0000

    Conditional battleground backgrounds

--- src/scenes/battle/BattleView.js
+++ src/scenes/battle/BattleView.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import { View, Image } from 'react-native';
+import React, { useContext } from 'react';
+import { View, Image, StyleSheet } from 'react-native';
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { GameContext } from '../../contexts/GameContext.js'
 
 const BattleView = () => {
+
+  const { level } = useContext(GameContext)
+
+  const background = () => {
+    if (level == 4) {
+      return <Image style={styles.background} source={require('../../../assets/battle-background.png')} />
+    } else if (level == 6) {
+      return <Image style={styles.background} source={require('../../../assets/battle-background-fence.png')} />
+    }
+  }
+
   return (
     < View
       style={{
@@ -11,17 +23,17 @@ const BattleView = () => {
         backgroundColor: 'rgb(199, 252, 177)',
         zIndex: '1'
       }}>
-      <Image
-        style={{
-          zIndex: 2,
-          width: wp("100%"),
-          height: hp("61.58%"),
-          resizeMode: "stretch"
-        }}
-        source={require('../../../assets/battle-background-fence.png')}
-      />
+      {background()}
     </View>
   )
-
 }
 export default BattleView;
+
+const styles = StyleSheet.create({
+  background: {
+    zIndex: 2,
+    width: wp("100%"),
+    height: hp("61.58%"),
+    resizeMode: "stretch"
+  }
+})
